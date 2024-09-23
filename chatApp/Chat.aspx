814abf59d6d85c146d67f5e457be60333ea1219b
Riya Jivani <riyajivani8@gmail.com>
2024-09-23 23:14:24 +0530

    chatapp

---
+++ chatApp/Chat.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Chat.aspx.cs" Inherits="chatApp.Chat" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        #chatArea {
+            height: 300px;
+            overflow-y: scroll;
+            border: 1px solid #ccc;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Chat Application</h2>
+            <div id="chatArea" runat="server"></div>
+            <asp:TextBox ID="txtUsername" runat="server" Placeholder="Enter your username"></asp:TextBox><br />
+            <asp:TextBox ID="txtMessage" runat="server" Placeholder="Enter your message"></asp:TextBox><br />
+            <asp:Button ID="btnSend" runat="server" Text="Send" OnClick="btnSend_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ chatApp/Chat.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace chatApp
+{
+    public partial class Chat : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DisplayMessages();
+            }
+        }
+
+        private void DisplayMessages()
+        {
+            if (Application["Messages"] != null)
+            {
+                var messages = (List<string>)Application["Messages"];
+                chatArea.InnerHtml = string.Join("<br />", messages);
+            }
+        }
+
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string message = txtMessage.Text.Trim();
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(message))
+            {
+                string formattedMessage = $"{username}: {message}";
+
+                Application.Lock();
+                if (Application["Messages"] == null)
+                {
+                    Application["Messages"] = new List<string>();
+                }
+                var messages = (List<string>)Application["Messages"];
+                messages.Add(formattedMessage);
+                Application["Messages"] = messages; 
+                Application.UnLock();
+
+                txtMessage.Text = string.Empty;
+
+                DisplayMessages();
+            }
+           }
+    }
+}
